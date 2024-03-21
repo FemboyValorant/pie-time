56f593a9832caabe7e76fc1671e4c10edda231f4
FemboyValorant <matt.sant143@gmail.com>
2024-03-21 08:48:22 -0700

    Implement pause button

--- chart/script.js
+++ chart/script.js
@@ -1,19 +1,37 @@
-// 1 minute = 60000 milliseconds
-totalTime = 60000
-pieInterval = 
+// 1 minute = 60 seconds = 60000 milliseconds
+let totalMinutes = 2;
+let totalMilliseconds = totalMinutes * 1000 * 60;
+let pauseFlag = false;
 
-chart = document.querySelector("#cover");
+const chart = document.querySelector("#cover");
+
+const pauseButton = document.querySelector("#pause");
+pauseButton.addEventListener('click', () => {
+    switch (true){
+        case pauseFlag === false:
+            console.log("Paused")
+            pauseFlag = true;
+            break;
+        default:
+            console.log("Unpaused");
+            pauseFlag = false;
+            break;
+    }
+})
+
 let piePercent = 0;
+var t = setInterval(() => {
 
-    
-    var t = setInterval(() => {
+    if (pauseFlag === false){
         chart.style.background = "conic-gradient( #8A9A5B " + piePercent + "%, rgba(255, 0, 0, 0) 0)";
-        piePercent += 0.025;
-        if (piePercent > 100){
+        piePercent += 0.2;
+        if (piePercent > 105){
             clearInterval(t);
         }
+    }
+
         
-    }, 20) //1/50th of a second 
+}, totalMilliseconds/500) 
 
 
 
